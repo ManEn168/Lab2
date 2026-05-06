25ef81daa6494f8d554759294be0796da90ae41f
M_E168 <Manen.25@ichat.sp.edu.sg>
2026-05-06 09:10:11 +0800

    Update file

--- bmi.py
+++ bmi.py
@@ -4,22 +4,21 @@
 
 #calculate_bmi(weight=" 57", height=" 1.73")    
 
-
-
 def calculate_bmi(height,weight):
     print("Height =" + str(height))
     print("Weight =" + str(weight))
 
     calculate_bmi = weight / (height*height)
-    print (calculate_bmi)
+    print(calculate_bmi)
 
     if calculate_bmi < 18.5:
-        print("Underweight")
+        print("Under Weight")
+        return -1
     elif (calculate_bmi >=18.5) and (calculate_bmi<=25.0):
         print("Normal Weight")
+        return 0
     elif calculate_bmi>25.0:
         print("OverWeight")
-
-
+        return 1
 
 calculate_bmi(weight= 57, height= 1.73)    
